3cff65c6a5bde42f94497d26ea0ae928e38b51f3
Bhavesh Ramburn <nitrogen@gmail.com>
2020-03-08 14:13:05 +0000

    Update portfile.cmake

--- ports/wiringpi/portfile.cmake
+++ ports/wiringpi/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO bramburn/wiringpi
-        REF v2.6.1
+        REF v2.6.3
         SHA512 d8bd44439100772929eb8a4eb4aebfd66fa54562c838eb4c081a382dc1d73c545faa6d9675e320864d9b533e4a0c4a673e44058c7f643ccd56ec90830cdfaf45
         HEAD_REF master
 )
